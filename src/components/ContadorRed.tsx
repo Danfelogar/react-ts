bf7edb167225ac15542fd54c4350d41f44cccc80
Daniel Felipe <danypolo18@hotmail.com>
2021-12-24 18:29:57 -0500

    4commit learneing 4 class

--- src/components/ContadorRed.tsx
+++ src/components/ContadorRed.tsx
@@ -1,8 +1,59 @@
+import { useReducer } from "react";
+
+const initialState ={
+    contador: 10,
+}
+
+type ActionType =
+    | {type: 'incrementar'}
+    | {type: 'decrementar'}
+    | {type: 'custom', payload: number};
+
+const contadorReducer = ( state: typeof initialState, action: ActionType ) => {
+
+    switch (action.type) {
+        case 'incrementar':
+            return{
+                ...state,
+                contador: state.contador + 1
+            }
+        case 'decrementar':
+            return{
+                    ...state,
+                contador: state.contador - 1
+            }
+            case 'custom':
+                return{
+                        ...state,
+                    contador: action.payload
+                }
+        default:
+            return state
+    }
+}
 
 export const ContadorRed = () => {
+
+    const [{ contador }, dispatch] = useReducer(contadorReducer, initialState)
+
     return (
         <>
-            <h3>Contador: 10</h3>
+            <h3>Contador: { contador }</h3>
+            <button
+            onClick={ ()=> dispatch({type: 'incrementar'}) }
+            className="btn btn-outline-primary">
+                +1
+            </button>
+            <button
+            onClick={ ()=> dispatch({type: 'decrementar'}) }
+            className="btn btn-outline-primary">
+                -1
+            </button>
+            <button
+            onClick={ ()=> dispatch({type: 'custom',payload: 100}) }
+            className="btn btn-outline-danger">
+                100
+            </button>
         </>
     )
 }
